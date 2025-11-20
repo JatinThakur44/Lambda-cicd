47bb4309b3ef899ed8816cc9e3f2b44351c4f036
JatinThakur44 <jatinthakur9193@gmail.com>
2025-11-20 21:58:41 +0530

    final commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,6 +7,7 @@ terraform {
     key            = "lambda/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    use_lockfile   = true
+    
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,5 @@
 
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
+/*
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "${var.lambda_function_name}-role"
@@ -72,6 +62,92 @@ resource "aws_lambda_function" "main" {
 }
 
 # CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "lambda_logs1" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 7
+
+  tags = {
+    Environment = var.environment
+  }
+}
+*/
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# --------------------------------------
+# IAM Role for Lambda
+# --------------------------------------
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.lambda_function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+# Attach basic execution policy
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+# --------------------------------------
+# Lambda Function (ZIP created in CI/CD)
+# --------------------------------------
+resource "aws_lambda_function" "main" {
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+
+  # ZIP created by GitHub Actions and placed in terraform folder before apply
+  filename         = "${path.module}/lambda_function.zip"
+
+  # hash ensures updates trigger new deployment
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
+
+  #handler         = "index.handler"          # Node.js
+  handler          = "index.lambda_handler"   # Python
+
+  runtime          = "python3.11"
+  timeout          = 30
+  memory_size      = 128
+
+  environment {
+    variables = {
+      ENVIRONMENT = var.environment
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+# --------------------------------------
+# CloudWatch Log Group
+# --------------------------------------
 resource "aws_cloudwatch_log_group" "lambda_logs1" {
   name              = "/aws/lambda/${var.lambda_function_name}"
   retention_in_days = 7
