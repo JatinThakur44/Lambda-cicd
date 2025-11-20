ce32ee4912f86075b0b1cd2abb92c3c7af9f851b
JatinThakur44 <jatinthakur9193@gmail.com>
2025-11-20 21:21:23 +0530

    3rd  apply runnnn with backend

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 }
 
 variable "lambda_function_name" {
-  description = "Lambda function nname"
+  description = "Lambda function nnakme"
   type        = string
   default     = "my-simple-lambda"
 }
