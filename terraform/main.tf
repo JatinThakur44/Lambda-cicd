e3f30f4eaaae3f308bec574ed2a4e08bd4b4339a
JatinThakur44 <jatinthakur9193@gmail.com>
2025-11-24 22:53:04 +0530

    1st Run

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,77 +1,4 @@
 
-/*
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.lambda_function_name}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = {
-    Environment = var.environment
-  }
-}
-
-# Attach basic execution policy
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda_role.name
-}
-
-# Create zip file from lambda code
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambda"
-  output_path = "${path.module}/lambda_function.zip"
-}
-
-# Lambda Function
-resource "aws_lambda_function" "main" {
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = var.lambda_function_name
-  role            = aws_iam_role.lambda_role.arn
-  #handler         = "index.handler"  # For Node.js
-  handler       = "index.lambda_handler"  # For Python
-  
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  
-  runtime = "python3.11"  # "nodejs18.x"  
-  timeout = 30
-  memory_size = 128
-
-  environment {
-    variables = {
-      ENVIRONMENT = var.environment
-    }
-  }
-
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
-
-# CloudWatch Log Group
-resource "aws_cloudwatch_log_group" "lambda_logs1" {
-  name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 7
-
-  tags = {
-    Environment = var.environment
-  }
-}
-*/
-
 terraform {
   required_version = ">= 1.0"
 
@@ -82,7 +9,6 @@ terraform {
     }
   }
 }
-
 # --------------------------------------
 # IAM Role for Lambda
 # --------------------------------------
